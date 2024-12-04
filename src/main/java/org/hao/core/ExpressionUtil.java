6a3f6fe541ea6a7fc919086bf7a6a8835a13ec5e
wanghao <kshelloworldwh@gmail.com>
2024-12-04 14:43:16 +0800

    feat(core): 优化 ExpressionUtil 类并添加新功能
    - 添加获取变量解析工厂和内置函数副本的方法
    - 新增多个执行表达式的重载方法，提供更灵活的使用方式
    - 添加构建解析上下文和执行表达式的方法
    - 新增添加全局方法和变量的功能
    - 优化类结构，增加必要的注释说明

--- src/main/java/org/hao/core/ExpressionUtil.java
+++ src/main/java/org/hao/core/ExpressionUtil.java
@@ -27,24 +27,46 @@ import java.util.stream.Collectors;
  */
 public class ExpressionUtil {
 
-
+    // 初始化规则模板
     public static final String rulesTemplate = initTemplate();
+    // 内置函数映射
     private static final Map<String, Method> builtInFunc = initBuiltInFuncMap();
+    // 变量解析工厂
     private static final Map<String, Object> variableResolverFactory = initVariableResolverFactory();
 
+    /**
+     * 获取变量解析工厂的副本
+     *
+     * @return 变量解析工厂的副本
+     */
     public static Map<String, Object> getVariableResolverFactory() {
         return new HashMap<>(variableResolverFactory);
     }
 
+    /**
+     * 获取内置函数的副本
+     *
+     * @return 内置函数的副本
+     */
     public static Map<String, Object> getBuiltInFunc() {
         return new HashMap<>(builtInFunc);
     }
 
+    /**
+     * 初始化模板
+     *
+     * @return 从类路径中读取模板文件并返回内容
+     */
     private static String initTemplate() {
         InputStream inputByClassPath = HutoolPlus.getInputByClassPath("META-INF/rulesTemplate.js");
         return IoUtil.read(inputByClassPath, StandardCharsets.UTF_8);
     }
 
+    /**
+     * 初始化变量解析工厂
+     *
+     * @return 变量解析工厂
+     */
     private static Map<String, Object> initVariableResolverFactory() {
         if (variableResolverFactory == null) {
             HashMap<String, Object> variableResolverFactory = new HashMap<>();
@@ -56,23 +78,53 @@ public class ExpressionUtil {
         return variableResolverFactory;
     }
 
+    /**
+     * 初始化内置函数映射
+     *
+     * @return 内置函数映射
+     */
     private static Map<String, Method> initBuiltInFuncMap() {
         Method[] methods = ReflectUtil.getMethodsDirectly(ExpressionBuiltInFunc.class, false, false);
         return Maps.asMap(Arrays.stream(methods).map(method -> Maps.put(method.getName(), method)).collect(Collectors.toList()), Method.class);
     }
 
+    /**
+     * 构建表达式
+     *
+     * @param expression 原始表达式
+     * @return 将规则模板和表达式合并后的结果
+     */
     private static String buildExpression(String expression) {
         return rulesTemplate + "\n " + expression;
     }
 
+    /**
+     * 评估表达式
+     *
+     * @param expression 表达式
+     * @param vars        变量
+     * @return 评估结果
+     */
     public static Object eval(String expression, Map<String, Object> vars) {
         return MVEL.eval(buildExpression(expression), vars);
     }
 
+    /**
+     * 评估表达式
+     *
+     * @param expression 表达式
+     * @param resolverFactory 变量解析工厂
+     * @return 评估结果
+     */
     public static Object eval(String expression, VariableResolverFactory resolverFactory) {
         return MVEL.eval(buildExpression(expression), resolverFactory);
     }
 
+    /**
+     * 构建解析上下文
+     *
+     * @return 解析上下文
+     */
     public static ParserContext buildParserContext() {
         ParserContext parserContext = ParserContext.create();
         builtInFunc.forEach((methodName, method) -> parserContext.addImport(methodName, method));
@@ -80,22 +132,57 @@ public class ExpressionUtil {
         return parserContext;
     }
 
+    /**
+     * 执行表达式
+     *
+     * @param expression 表达式
+     * @param parserContext 解析上下文
+     * @return 执行结果
+     */
     public static Object executeExpression(String expression, ParserContext parserContext) {
         return executeExpression(expression, parserContext, getVariableResolverFactory());
     }
 
+    /**
+     * 执行表达式
+     *
+     * @param expression 表达式
+     * @return 执行结果
+     */
     public static Object executeExpression(String expression) {
         return executeExpression(expression, buildParserContext(), getVariableResolverFactory());
     }
 
+    /**
+     * 执行表达式
+     *
+     * @param expression 表达式
+     * @param vars        变量
+     * @return 执行结果
+     */
     public static Object executeExpression(String expression, Map<String, Object> vars) {
         return executeExpression(expression, buildParserContext(), vars);
     }
 
+    /**
+     * 执行表达式
+     *
+     * @param expression 表达式
+     * @param resolverFactory 变量解析工厂
+     * @return 执行结果
+     */
     public static Object executeExpression(String expression, VariableResolverFactory resolverFactory) {
         return executeExpression(expression, buildParserContext(), resolverFactory);
     }
 
+    /**
+     * 执行表达式
+     *
+     * @param expression 表达式
+     * @param parserContext 解析上下文
+     * @param vars        变量
+     * @return 执行结果
+     */
     public static Object executeExpression(String expression, ParserContext parserContext, Map<String, Object> vars) {
         if (vars == null) {
             vars = getVariableResolverFactory();
@@ -104,6 +191,14 @@ public class ExpressionUtil {
         return MVEL.executeExpression(serializable, parserContext, vars);
     }
 
+    /**
+     * 执行表达式
+     *
+     * @param expression 表达式
+     * @param parserContext 解析上下文
+     * @param resolverFactory 变量解析工厂
+     * @return 执行结果
+     */
     public static Object executeExpression(String expression, ParserContext parserContext, VariableResolverFactory resolverFactory) {
         if (resolverFactory == null) {
             resolverFactory = new MapVariableResolverFactory(getVariableResolverFactory());
@@ -112,6 +207,12 @@ public class ExpressionUtil {
         return MVEL.executeExpression(serializable, parserContext, resolverFactory);
     }
 
+    /**
+     * 添加全局方法
+     *
+     * @param name 方法名
+     * @param method 方法对象
+     */
     public  void addGlobalMethod(String name, Method method) {
         if (builtInFunc.containsKey(name)) {
             throw new RuntimeException("name:" + name + " is already exist");
@@ -122,6 +223,12 @@ public class ExpressionUtil {
         builtInFunc.put(name, method);
     }
 
+    /**
+     * 添加全局变量
+     *
+     * @param name 变量名
+     * @param object 变量对象
+     */
     public  void addGlobalVariable(String name, Object object) {
         if (builtInFunc.containsKey(name)) {
             throw new RuntimeException("name:" + name + " is already exist");
