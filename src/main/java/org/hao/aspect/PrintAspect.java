12cb04c471b0012e62d8254a34f85cdee69f66d4
wanghao <kshelloworldwh@gmail.com>
2025-03-01 23:00:53 +0800

    feat(core): 新增 Excel 操作相关功能并优化列表生成
    - 新增 setStyleForRow 和 setStyle 方法，用于设置 Excel 行和区域样式
    - 新增 generateList 方法，用于生成指定长度和元素的列表
    - 更新 getExcelWriterByExportExcelVO 方法，简化调用
    - 优化自动调整列宽的方法 setSizeColumn
    - 移除未使用的导入和代码，提高代码可读性

--- src/main/java/org/hao/aspect/PrintAspect.java
+++ src/main/java/org/hao/aspect/PrintAspect.java
@@ -3,21 +3,13 @@ package org.hao.aspect;
 import cn.hutool.core.util.ObjectUtil;
 import io.swagger.annotations.ApiOperation;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.hao.annotation.PrintLnTime;
-import org.hao.core.ip.IPUtils;
-import org.hao.core.print.PrintUtil;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
-import java.text.SimpleDateFormat;
-import java.util.TimeZone;
 
 /**
  * Description:
--- src/main/java/org/hao/core/Lists.java
+++ src/main/java/org/hao/core/Lists.java
@@ -3,8 +3,32 @@ package org.hao.core;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class Lists {
+
+    /**
+     * 根据指定长度和元素生成器创建一个列表
+     * 此方法用于当需要创建一个特定长度的列表，并且列表中的每个元素都需要通过给定的生成器函数来生成时
+     * 通过使用泛型和函数式接口Supplier，此方法能够灵活地生成包含任何类型的元素的列表
+     *
+     * @param <T> 列表中元素的类型
+     * @param length 列表的长度，即列表中将包含的元素数量
+     * @param supplier 元素的生成器，用于生成列表中的每个元素
+     * @return 返回一个包含由生成器生成的元素的列表
+     */
+    public static <T> List<T> generateList(int length, Supplier<T> supplier) {
+        // 创建一个ArrayList实例来存储生成的元素
+        ArrayList<T> arrayList = new ArrayList<>();
+        // 循环指定次数，每次调用生成器来添加元素到列表中
+        for (int i = 0; i < length; i++) {
+            // 调用Supplier的get方法来生成一个新的元素，并添加到列表中
+            arrayList.add(supplier.get());
+        }
+        // 返回填充了生成元素的列表
+        return arrayList;
+    }
+
 
     public static <T> List<T> asList(T... values) {
         ArrayList<T> arrayList = new ArrayList<>();
